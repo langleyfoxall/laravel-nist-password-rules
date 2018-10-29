18f73ac50bc525e3d08c3d5c584390e4225dd124
Jordan Hall <jordan@hall05.co.uk>
2018-10-29 14:03:22 +0000

    Add context specific words dertivatives rule

---
+++ src/Rules/ContextSpecificWordsDerivatives.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace LangleyFoxall\LaravelNISTPasswordRules\Rules;
+
+use Illuminate\Contracts\Validation\Rule;
+
+/**
+ * Class ContextSpecificWordsDerivatives
+ *
+ * Implements the 'Context-specific words' recommendation with 'derivatives thereof'.
+ * from NIST SP 800-63B section 5.1.1.2.
+ */
+class ContextSpecificWordsDerivatives extends ContextSpecificWords implements Rule
+{
+    private $words = [];
+
+    /**
+     * Determine if the validation rule passes.
+     *
+     * @param  string $attribute
+     * @param  mixed $value
+     * @return bool
+     */
+    public function passes($attribute, $value)
+    {
+        $value = strtolower($value);
+
+        foreach($this->words as $word) {
+            if (stripos($value, $word) !== false) {
+                return false;
+            }
+
+            similar_text($value, $word, $percentage);
+            if ($percentage >= 75) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    /**
+     * Get the validation error message.
+     *
+     * @return string
+     */
+    public function message()
+    {
+        return 'The :attribute is similar to a context-specific word.';
+    }
+}
