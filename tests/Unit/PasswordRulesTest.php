6a2b09eaf75fd268579a17ea1813895c952f12d4
Jordan Hall <jordan@hall05.co.uk>
2018-10-30 12:51:33 +0000

    Test rule types

---
+++ tests/Unit/PasswordRulesTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace DivineOmega\PasswordExposed\Tests;
+
+use Illuminate\Contracts\Validation\Rule;
+use LangleyFoxall\LaravelNISTPasswordRules\PasswordRules;
+use PHPUnit\Framework\TestCase;
+
+class PasswordRulesTest extends TestCase
+{
+    public function passwordRulesProvider()
+    {
+        return [
+            [PasswordRules::register('username')],
+            [PasswordRules::changePassword('username', 'oldPassword')],
+            [PasswordRules::login()],
+        ];
+    }
+
+    /**
+     * @dataProvider passwordRulesProvider
+     */
+    public function testRuleTypes($passwordRules)
+    {
+        foreach($passwordRules as $rule) {
+            $validType = is_string($rule) || (is_object($rule) && $rule instanceof Rule);
+            $this->assertTrue($validType, 'Invalid rule type.');
+        }
+    }
+
+
+}
